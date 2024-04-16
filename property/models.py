a5a82daa7139f99573768eace3f99b6a7f5cfc52
busypenguin <112860511+busypenguin@users.noreply.github.com>
2024-04-16 20:23:04 +0300

    connect flats with owners

--- property/models.py
+++ property/models.py
@@ -5,9 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner_name = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', region='RU', blank=True)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.BooleanField(blank=True, null=True, default=None)
     created_at = models.DateTimeField(
         'Когда создано объявление',
